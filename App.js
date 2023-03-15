9df39faef9018d8be66688f1ab9f953340da496f
guibad22daw <guibad22daw@proton.me>
2023-03-15 20:01:01 +0100

    Afegit boto de sortir

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { Inici } from './components/Inici';
 import { Contacte } from './components/Contacte';
 import { Dades } from './components/Dades';
+import {CustomDrawer} from './components/CustomDrawer';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const Drawer = createDrawerNavigator();
@@ -12,7 +13,7 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator useLegacyImplementation initialRouteName="Home">
+      <Drawer.Navigator useLegacyImplementation initialRouteName="Home" drawerContent={props => <CustomDrawer {...props}/>}>
         <Drawer.Screen name="Inici" component={Inici} options={{drawerIcon: () => <Icon name="home" size={20} color={"#3b3b3b"} />}}/>
         <Drawer.Screen name="Dades" component={Dades} options={{drawerIcon: () => <Icon name="globe-outline" size={20} color={"#3b3b3b"} />}}/>
         <Drawer.Screen name="Contacte" component={Contacte} options={{drawerIcon: () => <Icon name="person" size={20} color={"#3b3b3b"} />}}/>

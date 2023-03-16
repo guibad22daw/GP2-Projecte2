40b89a20257aba7b0e1f61241eda784ec685357b
guibad22daw <15587039.clot@fje.edu>
2023-03-16 19:31:15 +0100

    Pantalla inicial millorada

--- components/Inici.js
+++ components/Inici.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export function Inici({ navigation }) {
+  const [loaded] = useFonts({
+    Poppins: require('../assets/fonts/Poppins-Regular.ttf'),
+    PoppinsMedium: require('../assets/fonts/Poppins-Medium.ttf'),
+    PoppinsSemibold: require('../assets/fonts/Poppins-SemiBold.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <ImageBackground 
       source={{uri: 'https://w0.peakpx.com/wallpaper/865/35/HD-wallpaper-africa-naturally.jpg'}}
       style={styles.backgroundImage}>
       <View style={styles.container}>
-        <Text style={styles.title}>Welcome to My App</Text>
+        <Text style={styles.title}>Pobresa mundial</Text>
         <Text style={styles.description}>
-          This is a sample description of my app.
+        A l'actualitat, moltes persones no coneixen la realitat actual de molts països. T'ajudem a descobrir-la.
         </Text>
         <Button
           title="Veure dades"
@@ -34,15 +45,16 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 32,
-    fontWeight: 'bold',
+    fontFamily:'PoppinsSemibold',
     color: '#fff',
     marginBottom: 20,
   },
   description: {
-    fontSize: 20,
+    fontFamily:'Poppins',
+    fontSize: 14,
     color: '#fff',
     textAlign: 'center',
     paddingHorizontal: 20,
-    marginBottom: 20,
+    marginBottom: 30,
   },
 });

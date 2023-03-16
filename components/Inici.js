f5362b149dbc5208b6df6f191cf99cb1db05fdc2
guibad22daw <15587039.clot@fje.edu>
2023-03-16 19:02:48 +0100

    Iniciada pantalla inicial

--- components/Inici.js
+++ components/Inici.js
@@ -1,13 +1,48 @@
-import { Button, View } from 'react-native';
-import estils from './Styles';
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
 
 export function Inici({ navigation }) {
   return (
-    <View style={estils.contenidor}>
-      <Button
-        onPress={() => navigation.navigate('Dades')}
-        title="Veure dades"
-      />
-    </View>
+    <ImageBackground 
+      source={{uri: 'https://w0.peakpx.com/wallpaper/865/35/HD-wallpaper-africa-naturally.jpg'}}
+      style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome to My App</Text>
+        <Text style={styles.description}>
+          This is a sample description of my app.
+        </Text>
+        <Button
+          title="Veure dades"
+          onPress={() => navigation.navigate('Dades')}
+        />
+      </View>
+    </ImageBackground>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 20,
+  },
+  description: {
+    fontSize: 20,
+    color: '#fff',
+    textAlign: 'center',
+    paddingHorizontal: 20,
+    marginBottom: 20,
+  },
+});

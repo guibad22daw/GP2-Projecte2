bd227cb931d0d8fcdb88a942ff3f6155376d036e
samreb22daw <samreb22daw@proton.me>
2023-03-23 11:14:24 +0100

    Graficos actualizados falta acabar

--- App.js
+++ App.js
@@ -17,11 +17,11 @@ db.transaction(tx => {
 
 db.transaction(
   tx => {
-    tx.executeSql("INSERT INTO continents VALUES('Unió Europea',446946712,21.7,90.98,100,80,38234.1)");
+    tx.executeSql("INSERT INTO continents VALUES('Unió Europea',446946712,21.7,90.98,100,80,38234)");
     tx.executeSql("INSERT INTO continents VALUES('Àfrica',1416625724,34,71,43,64.38,4121)");
-    tx.executeSql("INSERT INTO continents VALUES('Àsia',4598168800,27.5,84,97,79,13037.5)");
+    tx.executeSql("INSERT INTO continents VALUES('Àsia',4598168800,27.5,84,97,79,13037)");
     tx.executeSql("INSERT INTO continents VALUES('Àmerica del Nord',374241210,27.5,79,100,78,67514)");
-    tx.executeSql("INSERT INTO continents VALUES('Àmerica del Sud',439176795,30,76,98.5,78,8340.4)");
+    tx.executeSql("INSERT INTO continents VALUES('Àmerica del Sud',439176795,30,76,98.5,78,8340)");
     tx.executeSql("INSERT INTO continents VALUES('Oceania',44491724,26.9,98.44,99,83.2,48520)");
   }
 );

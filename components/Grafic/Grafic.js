b271a6ff1dccb567898897738014f49fc45fae33
guibad22daw <guibad22daw@proton.me>
2023-03-24 09:51:29 +0100

    Ja es fan varies consultes en SQL. Afegida consulta a gràfic

--- components/Grafic/Grafic.js
+++ components/Grafic/Grafic.js
@@ -1,23 +1,10 @@
-import React from "react";
-import {
-  StyleSheet,
-  Text,
-  ScrollView,
-  View,
-  ImageBackground,
-} from "react-native";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, ScrollView, View, ImageBackground } from "react-native";
+import * as SQLite from 'expo-sqlite';
 import { Chart } from "../Chart/Chart";
 import styles from "./Styles";
 
 // Dades de pobresa pel gràfic
-const dataPobresa = {
-  labels: ["Europa", "Àfrica", "Àsia", "Amèrica", "Oceania"],
-  datasets: [
-    {
-      data: [21.7, 34, 27.5, 28.8, 26.9],
-    },
-  ],
-};
 
 // Dades d'accés a electricitat pel gràfic
 const dataElectricitat = {
@@ -50,41 +37,74 @@ const dataRentaCapita = {
 };
 
 const Grafic = () => {
-  return (
-    <View>
-      <ImageBackground source={require("../../assets/img/grafics.jpg")}>
-        <ScrollView style={styles.scroll}>
-          <View style={{ flex: 1 }}>
-            <View style={styles.header}>
-              <Text style={styles.titol}>Gràfics comparatius</Text>
-              <Text style={styles.subtitol}>
-                Mostrem diversos gràfics comparatius amb les dades dels
-                diferents continents.
-              </Text>
+  const [Loading, setLoading] = useState(true);
+  let initialdataPobresa = {
+    labels: ["Europa", "Àfrica", "Àsia", "Amèrica", "Oceania"],
+    datasets: [
+      {
+        data: [],
+      },
+    ],
+  }
+
+  const [dataPobresa, setDataPobresa] = useState(initialdataPobresa)
+
+  useEffect(() => {
+    db = SQLite.openDatabase("db.db");
+    db.transaction(tx => {
+      tx.executeSql("select percPoverty from continents", [], async (_, { rows }) => {
+        let data = await rows._array;
+        let newData = [];
+        data.map(async (element) => {
+          newData.push(parseFloat(element.percPoverty));
+        });
+        initialdataPobresa.datasets[0].data = newData;
+        setDataPobresa(initialdataPobresa);
+        console.log('dataPobresa', dataPobresa)
+        setLoading(false);
+      })
+    });
+  }, [])
+
+    return (
+      <View>
+        <ImageBackground source={require("../../assets/img/grafics.jpg")}>
+          <ScrollView style={styles.scroll}>
+            <View style={{ flex: 1 }}>
+              <View style={styles.header}>
+                <Text style={styles.titol}>Gràfics comparatius</Text>
+                <Text style={styles.subtitol}>
+                  Mostrem diversos gràfics comparatius amb les dades dels
+                  diferents continents.
+                </Text>
+              </View>
             </View>
-          </View>
-          <View style={styles.container}>
-            <Text style={styles.titolGrafic}>Percentatge de pobresa</Text>
-            <Chart style={styles.chartContainer} data={dataPobresa} />
-          </View>
-          <View style={styles.container}>
-            <Text style={styles.titolGrafic}>
-              Percentatge d'accés a electricitat
-            </Text>
-            <Chart style={styles.chartContainer} data={dataElectricitat} />
-          </View>
-          <View style={styles.container}>
-            <Text style={styles.titolGrafic}>Esperança de vida</Text>
-            <Chart style={styles.chartContainer} data={dataEsperancaVida} />
-          </View>
-          <View style={styles.container}>
-            <Text style={styles.titolGrafic}>Renta per capita</Text>
-            <Chart style={styles.chartContainer} data={dataRentaCapita} />
-          </View>
-        </ScrollView>
-      </ImageBackground>
-    </View>
-  );
+            <View style={styles.container}>
+              <Text style={styles.titolGrafic}>Percentatge de pobresa</Text>
+              {
+                  Loading == false ?
+                  <Chart style={styles.chartContainer} data={dataPobresa} />
+                  : <Chart style={styles.chartContainer} data={initialdataPobresa} />
+              }
+            </View>
+            <View style={styles.container}>
+              <Text style={styles.titolGrafic}>
+                Percentatge d'accés a electricitat
+              </Text>
+              <Chart style={styles.chartContainer} data={dataElectricitat} />
+            </View>
+            <View style={styles.container}>
+              <Text style={styles.titolGrafic}>Esperança de vida</Text>
+              <Chart style={styles.chartContainer} data={dataEsperancaVida} />
+            </View>
+            <View style={styles.container}>
+              <Text style={styles.titolGrafic}>Renta per capita</Text>
+              <Chart style={styles.chartContainer} data={dataRentaCapita} />
+            </View>
+          </ScrollView>
+        </ImageBackground>
+      </View>
+    );
 };
 
 export default Grafic;
